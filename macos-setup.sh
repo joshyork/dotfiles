181df6ed80ad7a069fa8b16e82108248e2da6e22
Josh York <joshua.s.york@gmail.com>
2022-02-06 20:26:24 -0600

    ignore already installed casks

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Starting setup..."
+echo "START: XCode cli tools"
 # install xcode CLI
 xcode-select --install
+echo -e "END: XCode cli tools\n"
 
+
+echo "START: Homebrew"
 # Check for Homebrew to be present, install if it's missing
 if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Homebrew successfully installed"
 else
     echo "Homebrew already installed 👍"
 fi
+echo -e "END: Homebrew\n"
 
 # Update homebrew recipes
 brew update
@@ -30,18 +35,32 @@ FORMULAE=(
     tig
     zsh
 )
-CASKS=(
-    alfred
-    1password
-    google-chrome
-    visual-studio-code
-    todoist
-    postman
-    notion
-    kitty
-)
-echo "Installing Homebrew Formulae..."
+echo "START: Homebrew formulae"
 brew install ${FORMULAE[@]}
+echo -e "END: Homebrew formulae\n"
 
-echo "Installing Homebrew Casks..."
-brew install --cask ${CASKS[@]}
+CASKS=(
+    alfred,"Alfred 4"
+    1password,"1Password 7"
+    google-chrome,"Google Chrome"
+    visual-studio-code,"Visual Studio Code"
+    todoist,"Todoist"
+    postman,"Postman"
+    notion,"Notion"
+    kitty,"Kitty"
+)
+echo "START: Homebrew casks"
+(
+IFS=$'\n'
+for TOUPLE in ${CASKS[@]}; do
+    IFS=',';
+    set -- $TOUPLE
+    if [[ ! -d "/Applications/$2.app" ]]; then
+        echo "Installing $1 at $2"
+        brew install --cask $1
+    else
+        echo "$2 already installed. Skipping $1 cask."
+    fi
+done
+)
+echo "END: Homebrew casks"
