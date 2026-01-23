759aebabf7871df2a28dbbc08494f1861d388196
Josh York <joshua.s.york@gmail.com>
2026-01-23 15:31:50 -0700

    add cursor extensions

--- symlink-cursor-settings.sh
+++ symlink-cursor-settings.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ln -sf ~/code/personal/dotfiles/cursor/user-settings.json ~/"Library/Application Support/Cursor/User/settings.json"
+ln -sf ~/code/personal/dotfiles/cursor/extensions.json ~/.cursor/extensions/extensions.json
